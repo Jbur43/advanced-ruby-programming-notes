39a660dc6eb0ccca390e8250cf3467e6c7fdb38b
jbur43 <jtburum@gmail.com>
2022-06-18 10:44:15 -0400

    method_missing

---
+++ intercepting_errors_method_missing.rb
@@ -0,0 +1,35 @@
+# the concept of writing code that writes code.
+# method_missing gives us a way of interacting with objects / attributes that don't necessarily need
+# to be defined right away.
+
+# def method_missing(name, *args, &block)
+#   puts name, args, block
+#   # call rubys default handler
+#   # super
+# end
+
+# hello do end
+
+
+class User
+  attr_reader :attributes
+
+  def initialize
+    @attributes = {}
+  end
+
+  def method_missing(name, *args, &block)
+    # reason we are able to do this is because
+    # user.email = "x" is actually user.email=("x")
+    if name.end_with?("=")
+      @attributes[name.to_s[0..-2].to_sym] = args.first
+    else
+      @attributes[name]
+    end
+  end
+end
+
+user = User.new
+p user.email
+user.email = "test@test.com"
+p user.email
